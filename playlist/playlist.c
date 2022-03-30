47697ee23dc5d855ebd3f161330ce9e7803c1bc2
sehyun-kelly <sehyun.kelly.park@gmail.com>
2022-03-29 21:40:05 -0700

    implemented add, display, search

--- playlist/playlist.c
+++ playlist/playlist.c
@@ -1,93 +1,41 @@
-// A complete working C program to
-// demonstrate all insertion
-// methods
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-// A linked list node
-struct Node {
-    int data;
-    struct Node *next;
-    struct Node *prev;
-};
+typedef struct Playlist {
+    char* title;
+    char* singer;
+    struct Playlist *next;
+    struct Playlist *prev;
+}Playlist;
 
-/* Given a reference (pointer to pointer) to the head of a
-list and an int, inserts a new node on the front of the
-list. */
-void push_first(struct Node **head_ref, int new_data) {
-    /* 1. allocate node */
-    struct Node *new_node
-            = (struct Node *) malloc(sizeof(struct Node));
-    if (new_node == NULL) {
-        printf(" No memory is allocated.");
-        exit(0);
+void printSearch(bool exist, char* title, Playlist *dest);
+
+void fromTheTop(Playlist **album){
+    printf("before rewind: %s\n",(*album)->title);
+    while((*album)->prev != NULL){
+        *album = (*album) -> prev;
     }
 
-    /* 2. put in the data */
-    new_node->data = new_data;
-
-    /* 3. Make next of new node as head and previous as NULL
-    */
-    new_node->next = (*head_ref);
-    new_node->prev = NULL;
-
-    /* 4. change prev of head node to new node */
-    if ((*head_ref) != NULL)
-        (*head_ref)->prev = new_node;
-
-    /* 5. move the head to point to the new node */
-    (*head_ref) = new_node;
+    printf("after rewind: %s\n",(*album)->title);
 }
 
-/* Given a node as prev_node, insert a new node after the
-* given node */
-void push_after(struct Node *prev_node, int new_data) {
-    /*1. check if the given prev_node is NULL */
-    if (prev_node == NULL) {
-        printf("the given previous node cannot be NULL");
-        return;
-    }
-
-    /* 2. allocate new node */
-    struct Node *new_node
-            = (struct Node *) malloc(sizeof(struct Node));
-    if (new_node == NULL) {
-        printf(" No memory is allocated.");
-        exit(0);
-    }
-
-    /* 3. put in the data */
-    new_node->data = new_data;
-
-    /* 4. Make next of new node as next of prev_node */
-    new_node->next = prev_node->next;
-
-    /* 5. Make the next of prev_node as new_node */
-    prev_node->next = new_node;
-
-    /* 6. Make prev_node as previous of new_node */
-    new_node->prev = prev_node;
-
-    /* 7. Change previous of new_node's next node */
-    if (new_node->next != NULL)
-        new_node->next->prev = new_node;
-}
-
-/* Given a reference (pointer to pointer) to the head
-of a DLL and an int, appends a new node at the end */
-void push_last(struct Node **head_ref, int new_data) {
+void addSong(Playlist **album, char *title, char *singer) {
     /* 1. allocate node */
-    struct Node *new_node
-            = (struct Node *) malloc(sizeof(struct Node));
+    Playlist *new_node
+            = (Playlist *) malloc(sizeof(Playlist));
     if (new_node == NULL) {
         printf(" No memory is allocated.");
         exit(0);
     }
 
-    struct Node *last = *head_ref; /* used in step 5*/
+    Playlist *last = *album; /* used in step 5*/
 
     /* 2. put in the data */
-    new_node->data = new_data;
+    new_node->title = title;
+    new_node->singer = singer;
 
     /* 3. This new node is going to be the last node, so
         make next of it as NULL*/
@@ -95,9 +43,9 @@ void push_last(struct Node **head_ref, int new_data) {
 
     /* 4. If the Linked List is empty, then make the new
         node as head */
-    if (*head_ref == NULL) {
+    if (*album == NULL) {
         new_node->prev = NULL;
-        *head_ref = new_node;
+        *album = new_node;
         return;
     }
 
@@ -113,50 +61,199 @@ void push_last(struct Node **head_ref, int new_data) {
 
 }
 
-// This function prints contents of linked list starting
-// from the given node
-void printList(struct Node *node) {
-    struct Node *last;
-    printf("\nTraversal in forward direction \n");
-    while (node != NULL) {
-        printf(" %d ", node->data);
-        last = node;
-        node = node->next;
+void displayPlaylistReverse(Playlist *album){
+    Playlist *last = album;
+    Playlist *temp = album;
+
+    while (last->next != NULL) {
+        last = temp;
+        temp = temp->next;
     }
 
-    printf("\nTraversal in reverse direction \n");
+    printf("Playlist in Reverse:\n");
     while (last != NULL) {
-        printf(" %d ", last->data);
+        printf("%s, %s\n", last->title, last->singer);
         last = last->prev;
     }
+    printf("\n");
 }
 
-/* Driver program to test above functions*/
+void displayPlaylist(Playlist *album){
+    Playlist *first = album;
+    Playlist *temp = album;
+
+    while(first->prev != NULL){
+        first = temp;
+        temp = temp -> prev;
+    }
+
+    printf("Playlist:\n");
+    while (first != NULL) {
+        printf("%s, %s\n", first->title, first->singer);
+        first = first->next;
+    }
+    printf("\n");
+}
+
+void displayTest(Playlist *album){
+    Playlist *first = album;
+
+    printf("Playlist:\n");
+    while (first != NULL) {
+        printf("%s, %s\n", first->title, first->singer);
+        first = first->next;
+    }
+    printf("\n");
+}
+
+void currentSong(Playlist *album){
+    printf("Current Song:\n");
+    printf("%s, %s\n", album->title, album->singer);
+    printf("\n");
+}
+
+void nextSong(Playlist *album){
+    Playlist *temp = album;
+
+    printf("Next Song:\n");
+    if(temp->next == NULL){
+        printf("End of Playlist\n");
+    } else{
+        printf("%s, %s\n",temp->next->title, temp->next->singer);
+    }
+    printf("\n");
+}
+
+void previousSong(Playlist *album){
+    Playlist *temp = album;
+
+    printf("Previous Song:\n");
+    if(temp->prev == NULL){
+        printf("End of Playlist\n");
+    } else{
+        printf("%s, %s\n",temp->prev->title, temp->prev->singer);
+    }
+    printf("\n");
+}
+
+void searchSong(Playlist **album, char* title){
+    Playlist *temp = *album;
+    Playlist *dest = NULL;
+    bool exist = true;
+
+    while(temp->next != NULL) {
+        if (!strcmp(temp->title, title)) {
+            dest = temp;
+        }
+        temp = temp->next;
+    }
+
+    if(dest == NULL){
+        exist = false;
+    }
+
+    printSearch(exist, title, dest);
+}
+
+void printSearch(bool exist, char* title, Playlist *dest){
+    printf("Searched Song:\n");
+    if(!exist){
+        printf("%s, Does not exist\n", title);
+    }else{
+        printf("%s, %s\n", dest->title, dest->singer);
+    }
+    printf("\n");
+}
+
+bool deleteSearch(Playlist **album, char* title){
+    Playlist *temp = *album;
+
+    Playlist *dest = NULL;
+    bool exist = true;
+
+    while(temp->next != NULL) {
+        if (!strcmp(temp->title, title)) {
+            dest = temp;
+        }
+        temp = temp->next;
+    }
+
+    if(temp->next == NULL & !strcmp(temp->title, title)){
+        dest = temp;
+    }
+
+    if(dest == NULL){
+        exist = false;
+    }
+
+    return exist;
+}
+
+void deleteSong(Playlist **album, char* title){
+    printf("Song Deleted:\n");
+    if(*album == NULL){
+        printf("Playlist is empty.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    if(!deleteSearch(album, title)){
+        printf("%s, does not exist so cannot delete\n", title);
+        exit(EXIT_SUCCESS);
+    }
+    Playlist *dest = *album;
+
+    while(dest->next->next != NULL && strcmp(dest->title, title)) {
+        dest = dest->next;
+
+        if(dest->next->next == NULL && !strcmp(dest->title, title)){
+            dest = dest->next;
+        }
+    }
+
+
+
+    printf("%s, %s\n", dest->title, dest->singer);
+
+    if(!strcmp((*album)->title, title)){
+        *album = dest->next;
+        (*album)->prev = NULL;
+    }else {
+        if(dest->next != NULL){
+            dest->next->prev = dest->prev;
+        }
+
+        if(dest->prev != NULL){
+            dest->prev->next = dest->next;
+        }
+    }
+    printf("\n");
+
+}
+
 int main() {
-    /* Start with the empty list */
-    struct Node *head = NULL;
+    Playlist *album = NULL;
 
-    // Insert 6. So linked list becomes 6->NULL
-    push_first(&head, 6);
-
-    // Insert 7 at the beginning. So linked list becomes
-    // 7->6->NULL
-    push_first(&head, 7);
-
-    // Insert 1 at the beginning. So linked list becomes
-    // 1->7->6->NULL
-    push_first(&head, 1);
-
-    // Insert 4 at the end. So linked list becomes
-    // 1->7->6->4->NULL
-    push_last(&head, 4);
-
-    // Insert 8, after 7. So linked list becomes
-    // 1->7->8->6->4->NULL
-    push_after(head->next, 8);
-
-    printf("Created DLL is: ");
-    printList(head);
+    addSong(&album, "Memories", "Maroon 5");
+    addSong(&album, "With or Without You", "U2");
+    addSong(&album, "Nothing Else Matters", "Metallica");
+    addSong(&album, "Maki Madni", "The Derek Trucks Band");
+    addSong(&album, "Summer of 69", "Bryan Adams");
+    addSong(&album, "Another Brick in the Wall", "Pink Floyd");
+//    displayPlaylistReverse(album);
+//    displayPlaylist(album);
+//
+//    currentSong(album);
+//    nextSong(album);
+//    previousSong(album);
+//    searchSong(&album, "Ideal 30 days");
+//    searchSong(&album, "Maki Madni");
+    deleteSong(&album, "Memories");
+    displayPlaylist(album);
+//    deleteSong(&album, "Summer of 69");
+//    displayPlaylist(album);
+    deleteSong(&album, "Another Brick in the Wall");
+    displayPlaylist(album);
+//    displayPlaylistReverse(album);
 
     return 0;
 }
