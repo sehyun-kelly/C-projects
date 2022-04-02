1fc2803f64d15b5f3ed75891f15fb1f1e4e5a5af
sehyun-kelly <sehyun.kelly.park@gmail.com>
2022-04-01 21:47:49 -0700

    delete method implemented

--- playlist/playlist.c
+++ playlist/playlist.c
@@ -11,57 +11,74 @@ typedef struct Playlist {
     struct Playlist *prev;
 }Playlist;
 
+void addSong(Playlist **album, char *title, char *singer);
+void currentSong(Playlist *album);
+void searchSong(Playlist **album, char* title);
 void printSearch(bool exist, char* title, Playlist *dest);
+void displayPlaylist(Playlist *album);
+void displayPlaylistReverse(Playlist *album);
+void nextSong(Playlist *album);
+void previousSong(Playlist *album);
+void delete_first(Playlist **album);
+void delete_last(Playlist **album);
+void deleteSong(Playlist **album, char* title);
 
-void fromTheTop(Playlist **album){
-    printf("before rewind: %s\n",(*album)->title);
-    while((*album)->prev != NULL){
-        *album = (*album) -> prev;
+int main() {
+    Playlist *album = NULL;
+
+    addSong(&album, "Memories", "Maroon 5");
+    addSong(&album, "With or Without You", "U2");
+    addSong(&album, "Nothing Else Matters", "Metallica");
+    addSong(&album, "Maki Madni", "The Derek Trucks Band");
+    addSong(&album, "Summer of 69", "Bryan Adams");
+    addSong(&album, "Another Brick in the Wall", "Pink Floyd");
+    displayPlaylistReverse(album);
+    displayPlaylist(album);
+
+    currentSong(album);
+    nextSong(album);
+    previousSong(album);
+    searchSong(&album, "Ideal 30 days");
+    searchSong(&album, "Maki Madni");
+    deleteSong(&album, "Memories");
+
+    return 0;
+}
+
+void checkNull(Playlist **album){
+    if(*album == NULL){
+        printf("Playlist is empty!\n");
+        exit(EXIT_SUCCESS);
     }
-
-    printf("after rewind: %s\n",(*album)->title);
 }
 
 void addSong(Playlist **album, char *title, char *singer) {
-    /* 1. allocate node */
-    Playlist *new_node
-            = (Playlist *) malloc(sizeof(Playlist));
-    if (new_node == NULL) {
-        printf(" No memory is allocated.");
-        exit(0);
+    Playlist *new_album = (Playlist *) malloc(sizeof(Playlist));
+    if (new_album == NULL) {
+        printf("No memory is allocated.");
+        exit(EXIT_SUCCESS);
     }
 
-    Playlist *last = *album; /* used in step 5*/
+    Playlist *last = *album;
 
-    /* 2. put in the data */
-    new_node->title = title;
-    new_node->singer = singer;
+    new_album->title = title;
+    new_album->singer = singer;
+    new_album->next = NULL;
 
-    /* 3. This new node is going to be the last node, so
-        make next of it as NULL*/
-    new_node->next = NULL;
-
-    /* 4. If the Linked List is empty, then make the new
-        node as head */
     if (*album == NULL) {
-        new_node->prev = NULL;
-        *album = new_node;
+        new_album->prev = NULL;
+        *album = new_album;
         return;
     }
 
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-        last = last->next;
+    while (last->next != NULL) last = last->next;
 
-    /* 6. Change the next of last node */
-    last->next = new_node;
-
-    /* 7. Make last node as previous of new node */
-    new_node->prev = last;
-
+    last->next = new_album;
+    new_album->prev = last;
 }
 
 void displayPlaylistReverse(Playlist *album){
+    checkNull(&album);
     Playlist *last = album;
     Playlist *temp = album;
 
@@ -79,6 +96,7 @@ void displayPlaylistReverse(Playlist *album){
 }
 
 void displayPlaylist(Playlist *album){
+    checkNull(&album);
     Playlist *first = album;
     Playlist *temp = album;
 
@@ -95,24 +113,15 @@ void displayPlaylist(Playlist *album){
     printf("\n");
 }
 
-void displayTest(Playlist *album){
-    Playlist *first = album;
-
-    printf("Playlist:\n");
-    while (first != NULL) {
-        printf("%s, %s\n", first->title, first->singer);
-        first = first->next;
-    }
-    printf("\n");
-}
-
 void currentSong(Playlist *album){
+    checkNull(&album);
     printf("Current Song:\n");
     printf("%s, %s\n", album->title, album->singer);
     printf("\n");
 }
 
 void nextSong(Playlist *album){
+    checkNull(&album);
     Playlist *temp = album;
 
     printf("Next Song:\n");
@@ -125,6 +134,7 @@ void nextSong(Playlist *album){
 }
 
 void previousSong(Playlist *album){
+    checkNull(&album);
     Playlist *temp = album;
 
     printf("Previous Song:\n");
@@ -137,6 +147,7 @@ void previousSong(Playlist *album){
 }
 
 void searchSong(Playlist **album, char* title){
+    checkNull(album);
     Playlist *temp = *album;
     Playlist *dest = NULL;
     bool exist = true;
@@ -165,95 +176,66 @@ void printSearch(bool exist, char* title, Playlist *dest){
     printf("\n");
 }
 
-bool deleteSearch(Playlist **album, char* title){
-    Playlist *temp = *album;
+void delete_first(Playlist **album){
+    checkNull(album);
 
-    Playlist *dest = NULL;
-    bool exist = true;
+    if((*album)->next == NULL){
+        *album = NULL;
+    }else{
+        Playlist *temp = *album;
+        *album = (*album)->next;
+        (*album)->prev = NULL;
+        free(temp);
+    }
+}
 
-    while(temp->next != NULL) {
-        if (!strcmp(temp->title, title)) {
-            dest = temp;
+void delete_last(Playlist **album){
+    checkNull(album);
+
+    if((*album)->next == NULL){
+        *album = NULL;
+    }else{
+        Playlist * temp = *album;
+
+        while(temp->next != NULL){
+            temp = temp->next;
         }
-        temp = temp->next;
-    }
 
-    if(temp->next == NULL & !strcmp(temp->title, title)){
-        dest = temp;
+        temp->prev->next = NULL;
+        free(temp);
     }
-
-    if(dest == NULL){
-        exist = false;
-    }
-
-    return exist;
 }
 
 void deleteSong(Playlist **album, char* title){
+    checkNull(album);
     printf("Song Deleted:\n");
     if(*album == NULL){
         printf("Playlist is empty.\n");
         exit(EXIT_SUCCESS);
     }
 
-    if(!deleteSearch(album, title)){
+    Playlist *dest = *album;
+    Playlist *dest2 = NULL;
+
+    while(dest->next!= NULL && strcmp(dest->title, title)) {
+        dest = dest->next;
+    }
+
+    if(dest->next == NULL && strcmp(dest->title, title)){
         printf("%s, does not exist so cannot delete\n", title);
         exit(EXIT_SUCCESS);
     }
-    Playlist *dest = *album;
-
-    while(dest->next->next != NULL && strcmp(dest->title, title)) {
-        dest = dest->next;
-
-        if(dest->next->next == NULL && !strcmp(dest->title, title)){
-            dest = dest->next;
-        }
-    }
-
-
 
     printf("%s, %s\n", dest->title, dest->singer);
 
-    if(!strcmp((*album)->title, title)){
-        *album = dest->next;
-        (*album)->prev = NULL;
-    }else {
-        if(dest->next != NULL){
-            dest->next->prev = dest->prev;
-        }
-
-        if(dest->prev != NULL){
-            dest->prev->next = dest->next;
-        }
+    if(!strcmp(dest->title, title) && dest->prev == NULL) {delete_first(album);}
+    else if(!strcmp(dest->title, title) && dest->next == NULL){delete_last(album);}
+    else{
+        dest2 = dest->prev;
+        dest->next->prev = dest2;
+        dest2->next = dest->next;
+        free(dest);
     }
     printf("\n");
-
 }
 
-int main() {
-    Playlist *album = NULL;
-
-    addSong(&album, "Memories", "Maroon 5");
-    addSong(&album, "With or Without You", "U2");
-    addSong(&album, "Nothing Else Matters", "Metallica");
-    addSong(&album, "Maki Madni", "The Derek Trucks Band");
-    addSong(&album, "Summer of 69", "Bryan Adams");
-    addSong(&album, "Another Brick in the Wall", "Pink Floyd");
-//    displayPlaylistReverse(album);
-//    displayPlaylist(album);
-//
-//    currentSong(album);
-//    nextSong(album);
-//    previousSong(album);
-//    searchSong(&album, "Ideal 30 days");
-//    searchSong(&album, "Maki Madni");
-    deleteSong(&album, "Memories");
-    displayPlaylist(album);
-//    deleteSong(&album, "Summer of 69");
-//    displayPlaylist(album);
-    deleteSong(&album, "Another Brick in the Wall");
-    displayPlaylist(album);
-//    displayPlaylistReverse(album);
-
-    return 0;
-}
